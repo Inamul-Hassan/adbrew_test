43ec7cd6b0a41e49302c81d5ef52c7a2b6a176e7
Inamul Hassan <hassan.inamul@outlook.com>
2024-06-11 19:52:08 +0530

    Implement GET and POST methods
    - Using MongoDB client to perform GET and POST operations
    - Added created_at field to store the time of creation, for sorting the todo list in the order of creation while displaying in frontend

--- src/rest/todo/views.py
+++ src/rest/todo/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import json, logging, os, datetime
+from pymongo import MongoClient
 
-# Create your views here.
+mongo_uri = 'mongodb://' + os.environ["MONGO_HOST"] + ':' + os.environ["MONGO_PORT"]
+client = MongoClient(mongo_uri)
+
+class TodoListView(APIView):
+
+    def get(self, request):
+        # Implement this method - return all todo items from db instance above.
+        db = client['todo_db']
+        todo_collection = db['todos']
+        todos = list(todo_collection.find().sort('created_at', 1))
+        todos = [str(todo['_id']) for todo in todos]
+        return Response(todos, status=status.HTTP_200_OK)
+        
+    def post(self, request):
+        # Implement this method - accept a todo item in a mongo collection, persist it using db instance above.
+        db = client['todo_db']
+        todo_collection = db['todos']
+        todo_item = request.data
+        todo_item['created_at'] = datetime.datetime.now() 
+        result = todo_collection.insert_one(todo_item)
+        return Response({"inserted_id": str(result.inserted_id)}, status=status.HTTP_200_OK)
+     
+
