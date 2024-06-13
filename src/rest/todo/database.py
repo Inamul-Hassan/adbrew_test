1b42978c8b38dd744511a3cc7badc9e32cd9310b
Inamul Hassan <hassan.inamul@outlook.com>
2024-06-13 07:08:12 +0530

    Abstract database implementation
    - Decoupled database logic from backend views.
    - Added a separate database.py file, created an abstract TodoDatabase class, and implemented a MongoDBTodo class.

---
+++ src/rest/todo/database.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+from typing import List, Dict
+from pymongo import MongoClient
+import os
+import datetime
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class TodoDatabase(ABC):
+    @abstractmethod
+    def get_todos(self) -> List[Dict]:
+        pass
+
+    @abstractmethod
+    def add_todo(self, todo_item: Dict) -> str:
+        pass
+
+class MongoDbTodo(TodoDatabase):
+    def __init__(self):
+        mongo_uri = f"mongodb://{os.environ['MONGO_HOST']}:{os.environ['MONGO_PORT']}"
+        self.client = MongoClient(mongo_uri)
+        self.db = self.client['todo_db']
+        self.todo_collection = self.db['todos']
+
+    def get_todos(self) -> List[Dict]:
+      """
+      This method retrieves all todo items from the MongoDB.
+      """
+      try:
+        todos = list(self.todo_collection.find().sort('created_at', 1))
+        for todo in todos:
+            todo['_id'] = str(todo['_id'])
+        return todos
+      except Exception as e:
+        logger.error(f"Error retrieving todos: {e}")
+        raise
+
+
+    def add_todo(self, todo_item: Dict) -> str:   
+      """
+      This method accepts a todo item and persists it to the MongoDB.
+      """   
+      try:
+        # adding created_at field for sorting the todo list
+        todo_item['created_at'] = datetime.datetime.now()
+        result = self.todo_collection.insert_one(todo_item)
+        return str(result.inserted_id)
+      except Exception as e:
+        logger.error(f"Error creating todo: {e}")
+        raise
--- src/rest/todo/views.py
+++ src/rest/todo/views.py
@@ -2,38 +2,26 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import ParseError
 import logging
-import os
-import datetime
-from pymongo import MongoClient
+
+from .database import TodoDatabase, MongoDbTodo
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# MongoDB Client Config
-mongo_uri = f"mongodb://{os.environ['MONGO_HOST']}:{os.environ['MONGO_PORT']}"
-client = MongoClient(mongo_uri)
-db = client['todo_db']
-# Clear the databse collection
-# db.drop_collection('todos')
-todo_collection = db['todos']
-
 class TodoListView(APIView):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.todo_database: TodoDatabase = MongoDbTodo()
 
     def get(self, request):
         """
         Retrieve all todo items from the database.
         """
         try:
-            todos = list(todo_collection.find().sort('created_at', 1))
-            # If the collection is empty, return an empty list
-            if len(todos) == 0:
-                return Response([], status=status.HTTP_200_OK)
-            
-            # converting ObjectId to string
-            for todo in todos:
-                todo['_id'] = str(todo['_id'])
-                
+            todos = self.todo_database.get_todos()
             return Response(todos, status=status.HTTP_200_OK)
         
         except Exception as e:
@@ -47,14 +35,16 @@ class TodoListView(APIView):
         try:
             todo_item = request.data
             if not todo_item.get('todo'):
-                logger.error("Error creating todo: 'todo' field is required.")
-                return Response({"error": "The 'todo' field is required."}, status=status.HTTP_400_BAD_REQUEST)
-            
-            # adding created_at field for sorting the todo list
-            todo_item['created_at'] = datetime.datetime.now()
-            result = todo_collection.insert_one(todo_item)
-            return Response({"inserted_id": str(result.inserted_id)}, status=status.HTTP_201_CREATED)
+                raise ValueError("The 'todo' field is required in the request.")
+            inserted_id = self.todo_database.add_todo(todo_item)
+            return Response({"inserted_id": inserted_id}, status=status.HTTP_201_CREATED)
         
+        except ParseError as pe:
+            logger.error(f"Error creating todo: {pe}")
+            return Response({"error": "Invalid JSON format."}, status=status.HTTP_400_BAD_REQUEST)
+        except ValueError as ve:
+            logger.error(f"Error creating todo: {ve}")
+            return Response({"error": str(ve)}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             logger.error(f"Error creating todo: {e}")
             return Response({"error": "Failed to create todo."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
